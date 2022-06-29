a67ec73da19726024d5133a837a31088f8198226
Rishat Murzyev <rmurzyev@mail.ru>
2022-06-29 17:40:46 +0500

    Добавьте файлы проекта.

---
+++ InfoLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_App_SQL_WPF
+{
+    internal class InfoLog
+    {
+        public List<string> log = new();
+
+        public void AddToLog(string msg) => log.Add(msg);
+    }
+}
